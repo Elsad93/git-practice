18fb1534038758028f5045ba52f1cbac8e079786
Elshad Aghazada <elsad.agazade@gmail.com>
2021-04-09 21:29:04 -0700

    fixed issues found during review

--- solution-17.sql
+++ solution-17.sql
@@ -1,3 +1,5 @@
-select d.department_id, d.department_name
-from hr.departments d
-order by d.department_name
+select d.department_id, d.department_name,
+     d.manager_id, e.first_name as manager_first_name, e.last_name as manager_last_name
+from hr.departments d left join hr.employes e 
+    on d.manager_id = e.employee_id
+order by d.id
